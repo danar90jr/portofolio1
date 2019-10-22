805cf3c2633d8f26a47fcd58b7b5ded90850d1ba
danar90jr <danariakhoirul.anam@unida.gontor.ac.id>
2019-10-22 16:42:09 +0700

    YT api done

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+function get_CURL($url)
+{
+  $curl = curl_init();
+  curl_setopt($curl,CURLOPT_URL, $url);
+  curl_setopt($curl,CURLOPT_RETURNTRANSFER, 1);
+  $result = curl_exec($curl);
+  curl_close($curl);
+
+  return json_decode($result, true);
+
+}
+$result= get_CURL('https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCoK26y4iwTUCClqTT24SD6w&key=AIzaSyB2VZ4sJPgOhnwMhU4V9WxA0-QL8ijwtkk');
+
+$youtubeProfilePict = $result['items'][0]['snippet']['thumbnails']['medium']['url'];
+$channelName =  $result['items'][0]['snippet']['title'];
+$subscriber = $result['items'][0]['statistics']['subscriberCount'];
+
+//latesVideo
+$urlLatesVideo = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyB2VZ4sJPgOhnwMhU4V9WxA0-QL8ijwtkk&channelId=UCoK26y4iwTUCClqTT24SD6w&maxResults=1&order=date&part=snippet';
+$result = get_CURL($urlLatesVideo);
+$latesVideoId = $result['items'][0]['id']['videoId'];
+
+ ?>
 <!doctype html>
 <html lang="en" id="home">
 
@@ -113,6 +137,59 @@
     </div>
   </section>
 
+  <!-- SOSMED -->
+  <section class="social" id="social">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <h2>Social Media</h2>
+          <hr>
+          <br><br>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-5">
+          <div class="row">
+            <div class="col-md-4">
+              <img class="rounded-cyrcle" src="<?= $youtubeProfilePict;  ?>" alt="">
+            </div>
+            <div class="col-md-8">
+              <h5><?= $channelName  ?></h5>
+              <p><?= $subscriber ?> Subscribers</p>
+              <div class="g-ytsubscribe" data-channelid="UCoK26y4iwTUCClqTT24SD6w" data-layout="default" data-theme="dark" data-count="hidden"></div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col mt-4">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?= $latesVideoId ?>" allowfullscreen></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-5">
+          <div class="row">
+            <div class="col-md-4">
+              <img class="rounded-cyrcle" src="asset/img/profile1.png" alt="">
+            </div>
+            <div class="col-md-8">
+              <h5>Pokokmen Motovlog IG</h5>
+              <p>10000 Followers</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col mt-4">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
   <!-- Footer -->
   <footer>
     <div class="container">
@@ -130,6 +207,7 @@
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://apis.google.com/js/platform.js"></script>
 </body>
 
 </html>
